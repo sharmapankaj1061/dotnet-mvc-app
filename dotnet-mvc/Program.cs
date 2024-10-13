d235bc214d9abb91d612c2880d34a7ebce606edd
Pankaj Kumar Sharma <pankaj@mydeal.com.au>
2024-10-13 21:06:16 +1100

    Dummy commit to the repo will trigger an end-to-end pipeline

--- dotnet-mvc/Program.cs
+++ dotnet-mvc/Program.cs
@@ -24,4 +24,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+// this is a dummy commit to see if the deploymnet gets triggred.
+
 app.Run();
